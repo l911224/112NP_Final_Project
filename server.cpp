395463993b4a0c97f8542deeab67f29e5a7cbb05
l911224 <58912133+l911224@users.noreply.github.com>
2023-12-16 23:10:06 +0800

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,26 +1,23 @@
 #include "unp.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fstream>
-#include <iostream>
-using namespace std;
+#include <sys/time.h>
+#include <pthread.h>
 
 int numOfMember = 0;
-int watingRoomSockfd[4] = {0};
-char watingRoomUserID[4][MAXLINE];
-bool startGame = false;
-struct timeval timeout;
-timeout.tv_sec = 2;
-timeout.tv_usec = 0;
+int waitingRoomSockfd[4] = {0};
+char waitingRoomUserID[4][MAXLINE];
+int startGame = 0;
+struct timeval timeout = {2,0};
 
 void sig_chld(int signo);
-char* xchg_user_data(int sockfd,SA *cliaddr,socklen_t *clilen);
-void watingRoom();
-void gameRoom(int sockfd[4], char userID[4][MAXLINE]);
+void xchg_user_data(int sockfd, char * ID);
+void *waitingRoom(void * argv);
+void gameRoom(int sockfd[4], char *userID[4]);
 
-int main(int argc, char **argc){
-    int listenfd, connfd[4] = {0}, maxfd1 = -1;
-    SA cliaddr[4], servaddr;
+int main(int argc, char **argv){
+    int listenfd, connfd[4] = {0}, maxfdp1 = -1;
+    struct sockaddr_in cliaddr[4], servaddr;
     char userID[4][MAXLINE];
     socklen_t clilen[4];
     // Create listening socket
@@ -36,25 +33,21 @@ int main(int argc, char **argc){
     fd_set rset;
     Signal(SIGCHLD, sig_chld);
 
-    // Create wating room
-    if(Fork() == 0){
-        Close(listenfd);
-        watingRoom();
-        exit(0);
-    }
-    
+    // Create wating room thread
+    pthread_t wr;
+    pthread_create(&wr, NULL, waitingRoom, NULL);
+    printf("Thread created\n");
     for(;;){
-        
         if(startGame){
             if(Fork() == 0){    // Fork child process to start a game
-                Close(listenfd);
-                gameRoom(watingRoomSockfd, watingRoomUserID);
+                close(listenfd);
+                gameRoom(waitingRoomSockfd, waitingRoomUserID);
                 exit(0);
             }
-            startGame = false;
+            startGame = 0;
             numOfMember = 0;
-            memset(watingRoomSockfd, 0, sizeof(watingRoomSockfd));
-            memset(watingRoomUserID, 0, sizeof(watingRoomUserID));
+            memset(waitingRoomSockfd, 0, sizeof(waitingRoomSockfd));
+            memset(waitingRoomUserID, 0, sizeof(waitingRoomUserID));
             memset(connfd, 0, sizeof(connfd));
             memset(userID, 0, sizeof(userID));
             continue;
@@ -63,21 +56,29 @@ int main(int argc, char **argc){
         char sendline[MAXLINE], recvline[MAXLINE];
         FD_ZERO(&rset);
         FD_SET(listenfd, &rset);
-        maxfd1 = max(maxfd1, listenfd) + 1;
-        int sel = Select(maxfd1, &rset, 0, 0, &timeout);
+        maxfdp1 = -1;
+        maxfdp1 = max(maxfdp1, listenfd) + 1;
+        int sel = Select(maxfdp1, &rset, 0, 0, &timeout);
         if(sel == 0) continue;
 
         if(FD_ISSET(listenfd, &rset)){  // New connect arrived
             if ((connfd[numOfMember] = accept(listenfd, (SA *) &cliaddr[numOfMember], &clilen[numOfMember])) < 0) {
-                if (errno == EINTR) continue
-                else err_sys("accept error");
+                if (errno == EINTR) continue;
+                else perror("accept error");
 		    }
 
             if(Fork() == 0){    //Fork child process to get user ID
                 close(listenfd);
-                userID[numOfMember] = xchg_user_data(connfd[numOfMember]);
-                watingRoomUserID[numOfMember] = userID[numOfMember];
-                watingRoomSockfd[numOfMember] = connfd[numOfMember];
+                int num = numOfMember;
+                xchg_user_data(connfd[num],userID[num]);
+                strcpy(waitingRoomUserID[num], userID[num]);
+                waitingRoomSockfd[num] = connfd[num];
+                printf("Successful get data\n");
+                for(int i = 0; i < 4; i++){
+                    if(waitingRoomSockfd[i] == 0) continue;
+                    sprintf(sendline, "%s entered the room.\tThere are %d players waiting now.\n", userID[num], num + 1);
+                    Writen(waitingRoomSockfd[i], sendline, MAXLINE);
+                }
                 exit(0);
             }
             numOfMember++;
@@ -86,125 +87,140 @@ int main(int argc, char **argc){
 
     }
 
-    
+    pthread_join(wr, NULL);
 }
 
-void gameRoom(int sockfd[4], char userID[4][MAXLINE]){
+void gameRoom(int sockfd[4], char *userID[4]){
     char sendline[MAXLINE], recvline[MAXLINE];
     for(int i = 0;i < 4; i++){
-        Writen(sockfd[i], "Successfully enter a game!\n", MAXLINE);
-        Close(sockfd[i]);
+        Writen(sockfd[i], "Successfully entered a game!\n", MAXLINE);
+        close(sockfd[i]);
     }
     return;
 }
 
-void watingRoom(){
-    int maxfd1 = -1;
+void *waitingRoom(void *argv){
+    int maxfdp1 = -1;
     char sendline[MAXLINE], recvline[MAXLINE];
-    fd_set = rset;
+    fd_set rset;
     for(;;){
         FD_ZERO(&rset);
         memset(sendline, 0, sizeof(sendline));
         memset(recvline, 0, sizeof(recvline));
         for(int i = 0; i < 4; i++){
-            if(watingRoomSockfd[i] != 0){
-                FD_SET(watingRoomSockfd[i], &rset);
-                maxfd1 = max(maxfd1, watingRoomSockfd[i]);
+            if(waitingRoomSockfd[i] != 0){
+                FD_SET(waitingRoomSockfd[i], &rset);
+                maxfdp1 = max(maxfdp1, waitingRoomSockfd[i]);
             }
         }
         
-        int sel = Select(maxfd1, &rset, 0, 0, &timeout);
+        maxfdp1 += 1;
+        int sel = Select(maxfdp1, &rset, 0, 0, &timeout);
         if(sel == 0) continue;
 
         for(int i = 0;i < 4; i++){
-            if(FD_ISSET(watingRoomSockfd[i], &rset)){
-                if(Read(watingRoomSockfd[i],recvline,MAXLINE) == 0 || recvline[0] == '2'){  //Fin recvd, broadcast to all players in the wating room.
-                    Writen(watingRoomSockfd[i], "See you next time!\n", MAXLINE);
+            if(FD_ISSET(waitingRoomSockfd[i], &rset)){
+                if(Read(waitingRoomSockfd[i],recvline,MAXLINE) == 0 || recvline[0] == '2'){  //Fin recvd, broadcast to all players in the wating room.
+                    Writen(waitingRoomSockfd[i], "See you next time!\n", MAXLINE);
                     numOfMember--;
-                    Close(watingRoomSockfd[i]);
-                    watingRoomSockfd[i] = 0;    //Reset sockfd and id
-                    memset(watingRoomUserID[i], 0, sizeof(watingRoomUserID[i]));
+                    close(waitingRoomSockfd[i]);
+                    waitingRoomSockfd[i] = 0;    //Reset sockfd and id
+                    memset(waitingRoomUserID[i], 0, sizeof(waitingRoomUserID[i]));
                     for(int j = 0; j < 4; j++){ // Broadcast to all users.
-                        if(j == i || watingRoomSockfd[j] == 0) continue;
-                        sprintf(sendline, "User: %s has left the room.\n\nThere are %d people in the room, waiting for somebody to start a game.", numOfMember);
-                        Writen(watingRoomSockfd[j], sendline, MAXLINE);
+                        if(j == i || waitingRoomSockfd[j] == 0) continue;
+                        sprintf(sendline, "Player: %s has left the room.\n\nThere are %d players in the room, waiting for somebody to start a game.", numOfMember);
+                        Writen(waitingRoomSockfd[j], sendline, MAXLINE);
                     }
                     continue;
                 }
                 else if(recvline[0] == '1'){    // Start a game.
                     for(int j = 0; j < 4; j++){
-                        if(watingRoomSockfd[j] == 0) continue;
-                        Writen(watingRoomSockfd[j], "Game start!\n", MAXLINE);
-                        Close(watingRoomSockfd[j]);
+                        if(waitingRoomSockfd[j] == 0) continue;
+                        Writen(waitingRoomSockfd[j], "Game start!\n", MAXLINE);
+                        close(waitingRoomSockfd[j]);
                     }
-                    startGame = true;
+                    startGame = 1;
                     break;
                 }
 
-                sprintf(sendline, "(%s) %s", watingRoomUserID[i], recvline);
+                sprintf(sendline, "(%s) %s", waitingRoomUserID[i], recvline);
                 for(int j = 0;j < 4; j++){
-                    if(watingRoomSockfd[j] == 0 || i == j) continue;
-                    Writen(watingRoomSockfd[j], sendline, MAXLINE);
+                    if(waitingRoomSockfd[j] == 0 || i == j) continue;
+                    Writen(waitingRoomSockfd[j], sendline, MAXLINE);
                 }
             }
         }
     }
 }
 
-char* xchg_user_data(int sockfd){
+void xchg_user_data(int sockfd, char *ID){  //client ctrl+c not implemented yet
     char sendline[MAXLINE], recvline[MAXLINE];
     // Ask for user data
-    Writen(sockfd, "Welcome to Yahtzee !\n\n[1] Create a new account\t[2] Login account\n", MAXLINE);
+    Writen(sockfd, "Welcome to Yahtzee!\n\n[1] Create a new account\t[2] Login account\n", MAXLINE);
     Read(sockfd, recvline, MAXLINE);
-    memset(recvline, 0, MAXLINE);
-    if(recvline[0] == 1){   // Create new account
-        char ID[MAXLINE], password[MAXLINE], dataPath[MAXLINE];
+    if(recvline[0] == '1'){   // Create new account
+        NEW:
+        char password[MAXLINE], dataPath[MAXLINE], fileContent[MAXLINE];
         Writen(sockfd, "Enter your ID : ", MAXLINE);
         Read(sockfd, ID, MAXLINE);
         Writen(sockfd, "Enter your password : ", MAXLINE);
         Read(sockfd, password, MAXLINE);
-        ofstream f;
-        sprintf(dataPath, "/userdata/%s.txt", ID);
-        f.open(dataPath);
-        f << password << "0 0\n0 0\n0 0\n";
-        Writen(sockfd, "Create successfully !\n", MAXLINE);
-        f.close();
+        printf("ID %sPW %s\n",ID,password);
+        ID[strlen(ID) - 1] = 0;
+        FILE *f;
+        sprintf(dataPath, "userdata/%s.txt", ID);
+        printf("%s\n",dataPath);
+        f = fopen(dataPath, "w");
+        printf("Seccessful open file\n");
+        sprintf(fileContent, "%s%d %d\n%d %d\n%d %d\n", password,0,0,0,0,0,0);
+        fprintf(f, fileContent);
+        Writen(sockfd, "Create successfully!\n\n", MAXLINE);
+        fclose(f);
     }
-    else if(recvline[1] == 2){  // Login account and show winning rate
-        char ID[MAXLINE], password[MAXLINE], dataPath[MAXLINE];
+    else if(recvline[0] == '2'){  // Login account and show winning rate
+        printf("Login\n");
+        char password[MAXLINE], dataPath[MAXLINE];
         Writen(sockfd, "Enter your ID : ", MAXLINE);
         Read(sockfd, ID, MAXLINE);
-        Writen(sockfd, "Enter your password : ", MAXLINE);
-        Read(sockfd, password, MAXLINE);
-        ifstream f;
-        sprintf(dataPath, "/userdata/%s.txt", ID);
+        ID[strlen(ID) - 1] = 0;
+        FILE *f;
+        sprintf(dataPath, "userdata/%s.txt", ID);
         int twoWin, twoPlayed, threeWin, threePlayed, fourWin, fourPlayed;
-        f.open(dataPath);
-        f >> ID >> twoWin >> twoPlayed >> threeWin >> threePlayed >> fourWin >> fourPlayed;
-        sprintf(sendline, "Login successfuly !\nYour winning rate is showed below :\nTwo players : %.2f\t\tThree players : %.2f\t\tFour players : %.2f", twoWin/twoPlayed, threeWin/threePlayed, fourWin/fourPlayed);
+        f = fopen(dataPath, "r");
+        if(f == NULL){
+            Writen(sockfd, "Account does not exist. Please create a new one\n", MAXLINE);
+            goto NEW;
+        }
+        fscanf(f, "%s\n%d %d\n%d %d\n%d %d\n", &password,&twoWin, &twoPlayed, &threeWin, &threePlayed, &fourWin, &fourPlayed);
+        password[strlen(password)] = '\n';
+        PW:
+        memset(recvline, 0, sizeof(recvline));
+        Writen(sockfd, "Enter your password : ", MAXLINE);
+        Read(sockfd, recvline, MAXLINE);
+        if(strcmp(password, recvline) != 0) goto PW;
+        sprintf(sendline, "Login successfuly!\n\nYour win rate is showed below :\n\nTwo players : %.2f\t\tThree players : %.2f\t\tFour players : %.2f", twoWin/twoPlayed, threeWin/threePlayed, fourWin/fourPlayed);
         Writen(sockfd, sendline, MAXLINE);
-        f.close();
+        fclose(f);
     }
+    memset(recvline, 0, MAXLINE);
     memset(sendline, 0, MAXLINE);
     if(numOfMember == 0){
-        sprintf(sendline, "You are the first player !\nWait for 1 - 3 more players to start a game !\n");
+        sprintf(sendline, "You are the first player! Wait for 1 - 3 more players to start a game!\n");
     }
     else{
-        sprintf(sendline, "There are %d players in this room. Waiting for the room keeper to start a game !\n", numOfMember + 1);
+        sprintf(sendline, "There are %d players in waiting room. Wait for the room keeper to start a game!\n", numOfMember + 1);
     }
     Writen(sockfd, sendline, MAXLINE);
     memset(sendline, 0, MAXLINE);
-    Writen(sockfd, "In the wating room, you can type to chat with other players.\n\nPress [1] to start a game.\tPress[2] to exit the game.\n", MAXLINE);
-    close(sockfd);
-    return ID;
+    Writen(sockfd, "In the waiting room, you can type to chat with other players.\n\nPress [1] to start a game.\tPress[2] to exit the game.\n", MAXLINE);
 }
 
 void sig_chld(int signo)
 {
-        pid_t   pid;
-        int             stat;
+    pid_t   pid;
+    int  stat;
 
-        while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
-        ;
-        return;
+    while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
+    ;
+    return;
 }
