81ed1e238901a5d073f7db1be6c521ef46167cdc
Revol123 <gtjrgrgxogk@gmail.com>
2023-12-19 00:37:12 +0800

    updated

--- client.c
+++ client.c
@@ -1,11 +1,125 @@
 #include "unp.h"
 
-void od_set_cursor(int, int);
-void od_clr_scr();
-void od_disp_str(const char *);
-void draw_title();
-void draw_table();
-void xchg_data(FILE *, int);
+#define RED     "\x1b[;31;1m"
+#define GREEN   "\x1b[;32;1m"
+#define YELLOW  "\x1b[;33;1m"
+#define BLUE    "\x1b[;34;1m"
+#define PURPLE  "\x1b[;35;1m"
+#define CYAN    "\x1b[;36;1m"
+#define WHITE   "\x1b[;37;1m"
+
+void od_set_cursor(int row, int col) { printf("\x1B[%d;%dH", row, col); }
+
+void od_clr_scr() { printf("\x1B[2J"); }
+
+void od_disp_str_red(const char *str)    { printf(RED "%s", str); }
+void od_disp_str_green(const char *str)  { printf(GREEN "%s", str); }
+void od_disp_str_yellow(const char *str) { printf(YELLOW "%s", str); }
+void od_disp_str_blue(const char *str)   { printf(BLUE "%s", str); }
+void od_disp_str_purple(const char *str) { printf(PURPLE "%s", str); }
+void od_disp_str_cyan(const char *str)   { printf(CYAN "%s", str); }
+void od_disp_str_white(const char *str)  { printf(WHITE "%s", str); }
+
+void putxy(int row, int col, const char *str, const char *color) {
+    od_set_cursor(row, col);
+    printf("%s%s", color, str);
+}
+
+void draw_title(int x, int y) {
+    putxy(x    , y, "░░▒█░░▒█░█▀▀▄░▒█░▒█░▀▀█▀▀▒█▀▀▀█░▒█▀▀▀░▒█▀▀▀", CYAN); 
+    putxy(x + 1, y, "░░▒▀▄▄▄▀▒█▄▄█░▒█▀▀█░░▒█░░░▄▄▄▀▀░▒█▀▀▀░▒█▀▀▀", BLUE); 
+    putxy(x + 2, y, "░░░░▒█░░▒█░▒█░▒█░▒█░░▒█░░▒█▄▄▄█░▒█▄▄▄░▒█▄▄▄", PURPLE); 
+}
+
+void draw_table() {
+    od_set_cursor(4, 1);
+    od_disp_str_white("　┌─────────────┬─────────────┬──────────┬──────────┬──────────┬──────────┐\r\n");
+    od_disp_str_white("　│UPPER SECTION│HOW TO SCORE?│ PLAYER 1 │ PLAYER 2 │ PLAYER 3 │ PLAYER 4 │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Aces         │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Twos         │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Threes       │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Fours        │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Fives        │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Sixes        │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│NUM_TOTAL(NT)│  ========>  │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Bonus        │ (NT>63) +35 │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│UPPER TOTAL  │  ========>  │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┴──────────┴──────────┴──────────┴──────────┤\r\n");
+    od_disp_str_white("　│LOWER SECTION│                                                         │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┬──────────┬──────────┬──────────┬──────────┤\r\n");
+    od_disp_str_white("　│3 of a kind  │     Sum     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│4 of a kind  │     Sum     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Full House   │     +25     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Sm. Straight │     +30     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Lg. Straight │     +40     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│YAHTZEE      │     +50     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Chance       │     Sum     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│YAHTZEE BONUS│ (each) +100 │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│LOWER TOTAL  │  ========>  │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│GRAND TOTAL  │  ========>  │          │          │          │          │\r\n");
+    od_disp_str_white("　└─────────────┴─────────────┴──────────┴──────────┴──────────┴──────────┘\r\n");
+}
+
+void draw_cmd(int x, int y) {
+    putxy(x    , y, "┌──────────────────────┐", WHITE);
+    putxy(x + 1, y, "│ [ A-E ] Change dices │", WHITE);
+    putxy(x + 2, y, "│ [ ▲ ▼ ] Move         │", WHITE);
+    putxy(x + 3, y, "│ [Enter] Confirm      │", WHITE);
+    putxy(x + 4, y, "│                      │", WHITE);
+    putxy(x + 5, y, "│ [  Q  ] Quit Game    │", WHITE);
+    putxy(x + 6, y, "└──────────────────────┘", WHITE);
+}
+
+void xchg_data(FILE *fp, int sockfd) {
+    int maxfdp1;
+    char sendline[MAXLINE], recvline[MAXLINE];
+
+    fd_set rset;
+    FD_ZERO(&rset);
+
+    for (;;) {
+        FD_SET(fileno(fp), &rset);
+        FD_SET(sockfd, &rset);
+        maxfdp1 = max(fileno(fp), sockfd) + 1;
+        Select(maxfdp1, &rset, NULL, NULL, NULL);
+
+        if (FD_ISSET(sockfd, &rset)) {
+            if (Read(sockfd, recvline, MAXLINE) == 0) return;
+
+            printf("recv: %s", recvline);
+            fflush(stdout);
+
+            if (strcmp(recvline, "Successfully entered a game!\n\n") == 0) {
+                od_clr_scr();
+                draw_title();
+                draw_table();
+            }
+        }
+
+        if (FD_ISSET(fileno(fp), &rset)) {
+            if (Fgets(sendline, MAXLINE, fp) == NULL) return;
+            Writen(sockfd, sendline, strlen(sendline));
+        }
+    }
+}
 
 int main(int argc, char **argv) {
     int sockfd;
@@ -27,104 +141,5 @@ int main(int argc, char **argv) {
     exit(0);
 }
 
-void od_set_cursor(int row, int col) {
-    char msg_buf[10];
-    sprintf(msg_buf, "\x1B[%d;%dH", row, col);
-    printf(msg_buf);
-}
-
-void od_clr_scr() { printf("\x1B[2J"); }
-
-void od_disp_str(const char *str) { printf(str); }
-
-void draw_title() {
-    od_set_cursor(1, 1);
-    od_disp_str("　__   __ _    _   _ _____ __________ _____ \n\r");
-    od_disp_str("　\\ \\ / // \\  | | | |_   _|__  / ____| ____|\n\r");
-    od_disp_str("　 \\ V // _ \\ | |_| | | |   / /|  _| |  _|  \n\r");
-    od_disp_str("　  | |/ ___ \\|  _  | | |  / /_| |___| |___ \n\r");
-    od_disp_str("　  |_/_/   \\_\\_| |_| |_| /____|_____|_____|\n\r");
-}
-
-void draw_table() {
-    od_set_cursor(6, 1);
-    od_disp_str("　┌─────────────┬─────────┬────┬────┬────┬────┐\n\r");
-    od_disp_str("　│UPPER SECTION│  SCORE  │NO.1│NO.2│NO.3│NO.4│\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Aces         │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Twos         │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Threes       │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Fours        │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Fives        │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Sixes        │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│NUM_TOTAL(N) │========>│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Bonus        │(N>63)+35│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│UPPER TOTAL  │========>│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┴────┴────┴────┴────┤\n\r");
-    od_disp_str("　│LOWER SECTION│                             │\n\r");
-    od_disp_str("　├─────────────┼─────────┬────┬────┬────┬────┤\n\r");
-    od_disp_str("　│3 of a kind  │   Sum   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│4 of a kind  │   Sum   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Full House   │   +25   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Sm. Straight │   +30   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Lg. Straight │   +40   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│YAHTZEE      │   +50   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Chance       │   Sum   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│YAHTZEE BONUS│(per)+100│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│LOWER TOTAL  │========>│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│GRAND TOTAL  │========>│    │    │    │    │\n\r");
-    od_disp_str("　└─────────────┴─────────┴────┴────┴────┴────┘\n\r");
-}
-
-void xchg_data(FILE *fp, int sockfd) {
-    int maxfdp1;
-    char sendline[MAXLINE], recvline[MAXLINE];
-
-    fd_set rset;
-    FD_ZERO(&rset);
-
-    for (;;) {
-        FD_SET(fileno(fp), &rset);
-        FD_SET(sockfd, &rset);
-        maxfdp1 = max(fileno(fp), sockfd) + 1;
-        Select(maxfdp1, &rset, NULL, NULL, NULL);
-
-        if (FD_ISSET(sockfd, &rset)) {
-            if (Read(sockfd, recvline, MAXLINE) == 0) {
-                err_quit("str_cli: server terminated prematurely");
-            }
-
-            printf("recv: %s", recvline);
-            fflush(stdout);
 
 
-            if (strcmp(recvline, "Game start!\n") == 0) {
-                od_clr_scr();
-                draw_title();
-                draw_table();
-            }
-        }
-
-        if (FD_ISSET(fileno(fp), &rset)) {
-            if (Fgets(sendline, MAXLINE, fp) == NULL) return;
-            Writen(sockfd, sendline, strlen(sendline));
-        }
-    }
-}
--- test.c
+++ test.c
@@ -2,74 +2,128 @@
 
 #include "unp.h"
 
-void od_set_cursor(int row, int col) {
-    char msg_buf[10];
-    sprintf(msg_buf, "\x1B[%d;%dH", row, col);
-    printf(msg_buf);
-}
+#define RED     "\x1b[;31;1m"
+#define GREEN   "\x1b[;32;1m"
+#define YELLOW  "\x1b[;33;1m"
+#define BLUE    "\x1b[;34;1m"
+#define PURPLE  "\x1b[;35;1m"
+#define CYAN    "\x1b[;36;1m"
+#define WHITE   "\x1b[;37;1m"
+
+void od_set_cursor(int row, int col) { printf("\x1B[%d;%dH", row, col); }
 
 void od_clr_scr() { printf("\x1B[2J"); }
 
-void od_disp_str(const char *str) { printf(str); }
+void od_disp_str_red(const char *str)    { printf(RED "%s", str); }
+void od_disp_str_green(const char *str)  { printf(GREEN "%s", str); }
+void od_disp_str_yellow(const char *str) { printf(YELLOW "%s", str); }
+void od_disp_str_blue(const char *str)   { printf(BLUE "%s", str); }
+void od_disp_str_purple(const char *str) { printf(PURPLE "%s", str); }
+void od_disp_str_cyan(const char *str)   { printf(CYAN "%s", str); }
+void od_disp_str_white(const char *str)  { printf(WHITE "%s", str); }
 
-void draw_title() {
-    od_set_cursor(1, 1);
-    od_disp_str("　███████╗\n\r");
-    od_disp_str("　\\ \\ / // \\  | | | |_   _|__  / ____| ____|\n\r");
-    od_disp_str("　 \\ V // _ \\ | |_| | | |   / /|  _| |  _|  \n\r");
-    od_disp_str("　  | |/ ___ \\|  _  | | |  / /_| |___| |___ \n\r");
-    od_disp_str("　  |_/_/   \\_\\_| |_| |_| /____|_____|_____|\n\r");
+void putxy(int row, int col, const char *str, const char *color) {
+    od_set_cursor(row, col);
+    printf("%s%s", color, str);
+}
+
+void draw_title(int x, int y) {
+    putxy(x    , y, "░░▒█░░▒█░█▀▀▄░▒█░▒█░▀▀█▀▀▒█▀▀▀█░▒█▀▀▀░▒█▀▀▀", CYAN); 
+    putxy(x + 1, y, "░░▒▀▄▄▄▀▒█▄▄█░▒█▀▀█░░▒█░░░▄▄▄▀▀░▒█▀▀▀░▒█▀▀▀", BLUE); 
+    putxy(x + 2, y, "░░░░▒█░░▒█░▒█░▒█░▒█░░▒█░░▒█▄▄▄█░▒█▄▄▄░▒█▄▄▄", PURPLE); 
 }
 
 void draw_table() {
-    od_set_cursor(6, 1);
-    od_disp_str("　┌─────────────┬─────────┬────┬────┬────┬────┐\n\r");
-    od_disp_str("　│UPPER SECTION│  SCORE  │NO.1│NO.2│NO.3│NO.4│\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Aces         │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Twos         │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Threes       │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Fours        │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Fives        │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Sixes        │  Count  │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│NUM_TOTAL(N) │========>│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Bonus        │(N>63)+35│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│UPPER TOTAL  │========>│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┴────┴────┴────┴────┤\n\r");
-    od_disp_str("　│LOWER SECTION│                             │\n\r");
-    od_disp_str("　├─────────────┼─────────┬────┬────┬────┬────┤\n\r");
-    od_disp_str("　│3 of a kind  │   Sum   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│4 of a kind  │   Sum   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Full House   │   +25   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Sm. Straight │   +30   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Lg. Straight │   +40   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│YAHTZEE      │   +50   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│Chance       │   Sum   │    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│YAHTZEE BONUS│(per)+100│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│LOWER TOTAL  │========>│    │    │    │    │\n\r");
-    od_disp_str("　├─────────────┼─────────┼────┼────┼────┼────┤\n\r");
-    od_disp_str("　│GRAND TOTAL  │========>│    │    │    │    │\n\r");
-    od_disp_str("　└─────────────┴─────────┴────┴────┴────┴────┘\n\r");
+    od_set_cursor(4, 1);
+    od_disp_str_white("　┌─────────────┬─────────────┬──────────┬──────────┬──────────┬──────────┐\r\n");
+    od_disp_str_white("　│UPPER SECTION│HOW TO SCORE?│ PLAYER 1 │ PLAYER 2 │ PLAYER 3 │ PLAYER 4 │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Aces         │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Twos         │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Threes       │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Fours        │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Fives        │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Sixes        │    Count    │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│NUM_TOTAL(NT)│  ========>  │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Bonus        │ (NT>63) +35 │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│UPPER TOTAL  │  ========>  │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┴──────────┴──────────┴──────────┴──────────┤\r\n");
+    od_disp_str_white("　│LOWER SECTION│                                                         │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┬──────────┬──────────┬──────────┬──────────┤\r\n");
+    od_disp_str_white("　│3 of a kind  │     Sum     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│4 of a kind  │     Sum     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Full House   │     +25     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Sm. Straight │     +30     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Lg. Straight │     +40     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│YAHTZEE      │     +50     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│Chance       │     Sum     │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│YAHTZEE BONUS│ (each) +100 │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│LOWER TOTAL  │  ========>  │          │          │          │          │\r\n");
+    od_disp_str_white("　├─────────────┼─────────────┼──────────┼──────────┼──────────┼──────────┤\r\n");
+    od_disp_str_white("　│GRAND TOTAL  │  ========>  │          │          │          │          │\r\n");
+    od_disp_str_white("　└─────────────┴─────────────┴──────────┴──────────┴──────────┴──────────┘\r\n");
+}
+
+void draw_sys_msg(int x, int y) {
+    putxy(x     , y, "┌────────────────────────────────────────────────┐", WHITE);
+    putxy(x + 1 , y, "│               System Broadcast                 │", WHITE);
+    putxy(x + 2 , y, "│                                                │", WHITE);
+    putxy(x + 3 , y, "│                                                │", WHITE);
+    putxy(x + 4 , y, "│                                                │", WHITE);
+    putxy(x + 5 , y, "│                                                │", WHITE);
+    putxy(x + 6 , y, "│                                                │", WHITE);
+    putxy(x + 7 , y, "│                                                │", WHITE);
+    putxy(x + 8 , y, "│                                                │", WHITE);
+    putxy(x + 9 , y, "│                                                │", WHITE);
+    putxy(x + 10, y, "│                                                │", WHITE);
+    putxy(x + 11, y, "│                                                │", WHITE);
+    putxy(x + 12, y, "│                                                │", WHITE);
+    putxy(x + 13, y, "│                                                │", WHITE);
+    putxy(x + 14, y, "│                                                │", WHITE);
+    putxy(x + 15, y, "│                                                │", WHITE);
+    putxy(x + 16, y, "│                                                │", WHITE);
+    putxy(x + 17, y, "│                                                │", WHITE);
+    putxy(x + 18, y, "│                                                │", WHITE);
+    putxy(x + 19, y, "│                                                │", WHITE);
+    putxy(x + 19, y, "│                                                │", WHITE);
+    putxy(x + 20, y, "│                                                │", WHITE);
+    putxy(x + 21, y, "│                                                │", WHITE);
+    putxy(x + 22, y, "└────────────────────────────────────────────────┘", WHITE);
+}
+
+
+void draw_cmd(int x, int y) {
+    putxy(x    , y, "┌──────────────────────┐", WHITE);
+    putxy(x + 1, y, "│ [ 1-5 ] Change dices │", WHITE);
+    putxy(x + 2, y, "│ [ ▲ ▼ ] Move         │", WHITE);
+    putxy(x + 3, y, "│ [Enter] Confirm      │", WHITE);
+    putxy(x + 4, y, "│                      │", WHITE);
+    putxy(x + 5, y, "│ [  Q  ] Quit Game    │", WHITE);
+    putxy(x + 6, y, "└──────────────────────┘", WHITE);
 }
 
 int main(int argc, char **argv) {
     od_clr_scr();
-    draw_title();
+    draw_title(1, 16);
     draw_table();
+    draw_sys_msg(15, 77);
+    draw_cmd(38, 77);
+    od_set_cursor(47, 1);
 }
+ 
